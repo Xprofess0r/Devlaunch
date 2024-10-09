058313f7df0d905602283abe5b22551a37954252
shashi <shashi2017razz@gmail.com>
2024-10-09 14:31:38 +0530

    latest committ

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./Sections/Header.jsx";
+import Header from "./Sections/Header.jsx";//chnaged name
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
