dc00fd5ceb4bf7e32b418d48ff7d9d5a4de23c46
Bipin <bk3284488@gmail.com>
2024-10-09 14:40:01 +0530

    solved header err

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Header from "./Sections/Header.jsx";//chnaged name
 import Hero from "./sections/Hero.jsx";
 import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
@@ -6,20 +5,21 @@ import Faq from "./sections/Faq.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
 import Download from "./sections/Download.jsx";
 import Footer from "./sections/Footer.jsx";
+import HeaderSection from "./Sections/Headers.jsx";
 
 const App = () => {
-  return (
-    <main>
-      <Header />
-      <Hero />
-      <Features />
-      <Pricing />
-      <Faq />
-      <Testimonials />
-      <Download />
-      <Footer />
-    </main>
-  );
+	return (
+		<main>
+			<HeaderSection />
+			<Hero />
+			<Features />
+			<Pricing />
+			<Faq />
+			<Testimonials />
+			<Download />
+			<Footer />
+		</main>
+	);
 };
 
 export default App;
