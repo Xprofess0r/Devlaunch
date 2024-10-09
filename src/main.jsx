6c8a07ab7b6e69aabfe17e983ffcb1d8b56606e0
Bipin <bk3284488@gmail.com>
2024-10-09 14:49:59 +0530

    changed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
