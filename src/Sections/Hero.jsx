8d7c01d747192eeb190932d67a331d5ba0d239e1
shashi <shashi2017razz@gmail.com>
2024-10-14 12:16:13 +0530

    added globee

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,7 +1,7 @@
 import ReactTypingEffect from "react-typing-effect";
 import { Element, Link as LinkScroll } from "react-scroll";
 import Button from "../components/Button.jsx";
-import Globe from "../components/globe.jsx";
+import Globe from "../components/Globee.jsx";
 const Hero = () => {
   return (
     <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32"
